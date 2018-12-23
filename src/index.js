28313c6db6e0b2ef6eb1eca15f2f59175a4c440f
giustini <agiustinic@gmail.com>
2018-12-23 17:17:40 +0100

    Add players endpoints

--- src/index.js
+++ src/index.js
@@ -9,8 +9,32 @@ const port = process.env.port || 3000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/hello/:name', (req, res) => {
-    res.send({message: `Hello ${req.params.name}!`})
+app.get('/api/players', (req, res) => {
+    res.status(200).send({players: []})
+});
+
+app.get('/api/players/:playerId', (req, res) => {
+    res.status(200).send({
+        message: "Player created",
+        player: {playerId: req.params.playerId}
+    })
+});
+
+app.post('/api/players', (req, res) => {
+    console.log(req.body);
+    res.status(201).send(req.body)
+});
+
+app.put('/api/players/:playerId', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({
+        message: "Player updated",
+        player: {playerId: req.params.playerId}
+    })
+});
+
+app.delete('/api/players/:playerId', (req, res) => {
+    res.status(204).send();
 });
 
 app.listen(port, () => {
