c52dc09608fcb64fc5801134820051f26f9b1cdb
giustini <agiustinic@gmail.com>
2018-12-23 15:36:10 +0100

    First endpoint GET /hello

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const port = process.env.port || 3000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.get('/hello', (req, res) => {
+    res.send({message: "Hello world!"})
+});
+
 app.listen(port, () => {
     console.log(`Running API REST on http://localhost:${port}`);
 });
