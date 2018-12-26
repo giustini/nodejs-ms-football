cc258f76e0df1e06e1ad8206d387240f6133f8ee
giustini <agiustinic@gmail.com>
2018-12-26 22:34:06 +0100

    Reformatting

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const Player = require('./model/player');
 const app = express();
 const port = process.env.port || 3000;
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/api/players', (req, res) => {
@@ -88,7 +88,7 @@ mongoose.connect('mongodb://localhost:27017/football', {useNewUrlParser: true}, 
 
     if (err) return console.log("Error connecting to DB");
 
-    console.log("DB connection is OK")
+    console.log("DB connection is OK");
     app.listen(port, () => {
         console.log(`Running API REST on http://localhost:${port}`);
     });
