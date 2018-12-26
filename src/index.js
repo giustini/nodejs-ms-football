d2657fd6d09c86c06424ec5ec418a70c47362c05
giustini <agiustinic@gmail.com>
2018-12-26 22:27:39 +0100

    Add Player model Implement GET all players, GET player, POST, PUT and DELETE player

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const Player = require('./model/player');
+
 const app = express();
 const port = process.env.port || 3000;
 
@@ -11,31 +13,75 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/api/players', (req, res) => {
-    res.status(200).send({players: []})
+    Player.find({}, (err, players) => {
+
+        if (err) return res.status(500).send({ message: "Error retrieving players from DB" });
+
+        res.status(200).send({ players })
+    })
 });
 
 app.get('/api/players/:playerId', (req, res) => {
-    res.status(200).send({
-        message: "Player created",
-        player: {playerId: req.params.playerId}
-    })
+
+    let playerId = req.params.playerId;
+
+    Player.findById(playerId, (err, player) => {
+
+        if (err) res.status(500).send({ message: "Error retrieving player from DB" });
+        if (!player) res.status(404).send({ message: "Player not found" });
+
+        res.status(200).send({ player });
+    });
+
 });
 
 app.post('/api/players', (req, res) => {
-    console.log(req.body);
-    res.status(201).send(req.body)
+
+    let player = new Player();
+    player.name = req.body.name;
+    player.number = req.body.number;
+    player.age = req.body.age;
+    player.picture = req.body.picture;
+    player.role = req.body.role;
+
+    player.save((err, playerStored) => {
+
+        if (err) res.status(500).send({ message: "Error saving player to DB" });
+
+        res.status(201).send({ player: playerStored });
+    });
 });
 
 app.put('/api/players/:playerId', (req, res) => {
-    console.log(req.body);
-    res.status(200).send({
-        message: "Player updated",
-        player: {playerId: req.params.playerId}
-    })
+
+    let playerId = req.params.playerId;
+
+    let playerUpdated = req.body;
+
+    Player.findByIdAndUpdate(playerId, playerUpdated, (err, player) => {
+
+        if (err) res.status(500).send({ message: "Error updating player from DB" });
+
+        res.status(200).send({ message: "Player updated", playerUpdated });
+    });
+
 });
 
 app.delete('/api/players/:playerId', (req, res) => {
-    res.status(204).send();
+
+    let playerId = req.params.playerId;
+
+    Player.findById(playerId, (err, player) => {
+
+        if (err) res.status(500).send({ message: "Error deleting player from DB" });
+
+        player.remove(err => {
+            if (err) res.status(500).send({ message: "Error deleting player from DB" });
+
+            res.status(200).send({ message: "Player deleted from DB", player });
+        })
+    });
+
 });
 
 mongoose.connect('mongodb://localhost:27017/football', {useNewUrlParser: true}, (err, res) => {
---
+++ src/model/player.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PlayerSchema = Schema({
+    name: String,
+    number: Number,
+    age: Number,
+    picture: String,
+    role: { type: String, enum: ['GK', 'DF', 'MF', 'AT'] }
+});
+
+module.exports = mongoose.model('Player', PlayerSchema);
